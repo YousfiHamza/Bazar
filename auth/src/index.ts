3c689de05fc7c991b1e3f5f991110b2cee1c2cd6
Hamza Yousfi <hamza@flowdigitalstudio.com>
2021-07-25 01:23:48 +0100

    k8s initial setups

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,11 @@ import {json} from 'body-parser'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+    console.log('get req')
+    res.send('Hi There !')
+})
+
 app.listen(3000, () => {
-    console.log('🔥 Listening on port -> 3000 🔥 ')
+    console.log('🔥 Listening on port -> 3000 🔥 !')
 })
