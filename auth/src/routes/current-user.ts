b80ab16f07dab7f3983d99c95cfa00537e0fcc7b
Hamza Yousfi <yousfihamza22@gmail.com>
2021-07-31 20:33:49 +0100

    feat: Sign out + currentUser Middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,10 @@
+import { currentUser } from './../middlewares/current-user'
 import express from 'express'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-  console.log('get req')
-  res.send('Hi There !')
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser })
 })
 
 export { router as currentUserRouter }
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,8 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/api/users/signout', (req, res) => {
-  console.log('Hi There from Sign Out !')
-  res.send('Hi There from Sign Out !')
+  req.session = null
+  console.log('Sign Out Success !')
+  res.send({})
 })
 
 export { router as signOutRouter }
